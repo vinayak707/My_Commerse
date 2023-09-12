950d554c5c7f992cbdc896be45fe2dd364706e9b
vinayak707 <109481607+vinayak707@users.noreply.github.com>
2023-09-12 08:41:37 +0530

    Add files via upload

---
+++ FTest.java
@@ -0,0 +1,42 @@
+package Framework;
+
+import org.testng.annotations.Test;
+
+public class FTest {
+@Test
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("learning annotations");
+		
+}
+@Test
+	public static void Personal_loan() {
+		System.out.println("Personal loan");
+		
+	}
+	@Test
+	public static void Jeolary_loan() {
+		System.out.println("jeolary loan");
+		
+	}
+	@Test
+	public static void Propert_loan() {
+		System.out.println("Property loan");
+		
+	}
+	@Test
+	public static void Bike_loan() {
+		System.out.println("Bike loan");
+		
+	}
+	
+	@Test
+	public static void study_loan() {
+		System.out.println("stydy loan");
+		
+	}
+	
+	
+
+}
